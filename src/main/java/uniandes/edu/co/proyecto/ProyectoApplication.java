9a0e21f0b0008dd7263e97ebbbdbe9df2e312008
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2023-10-01 17:39:24 -0500

    Rerpository

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,13 +1,31 @@
 package uniandes.edu.co.proyecto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collection;
+import uniandes.edu.co.proyecto.modelo.Hotel;
+import uniandes.edu.co.proyecto.repositorio.HotelRepository;
 
 @SpringBootApplication
-public class ProyectoApplication {
+public class ProyectoApplication implements CommandLineRunner {
 
+	@Autowired
+	private HotelRepository hotelRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args){
+
+		Collection<Hotel> hoteles = hotelRepository.darHoteles();
+
+		for(Hotel h : hoteles){
+			System.out.println(h);
+		}
+		
 	}
 
 }
---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/HotelRepository.java
@@ -0,0 +1,35 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.proyecto.modelo.Hotel;
+
+public interface HotelRepository extends JpaRepository <Hotel, Integer>{
+    
+    @Query (value = "SELECT * FROM hoteles", nativeQuery = true)
+    Collection<Hotel> darHoteles();
+
+    @Query (value = "SELECT * FROM hoteles WHERE id :id", nativeQuery = true)
+    Hotel darHotel(@Param("id") int id);
+
+
+    @Modifying 
+    @Transactional
+    @Query (value = "INSERT INTO hoteles (id, nombre, ubicacion, estrellas) VALUES(hoteleros_sequence.nextval, :nombre, :ubicacion, :estrellas)", nativeQuery = true)
+    void insertarHotel(@Param ("nombre") String nombre, @Param ("ubicacion") String ubicacion, @Param ("estrellas") Integer estrellas);
+
+    @Modifying 
+    @Transactional
+    @Query (value = "UPDATE hoteles SET nombre =:nombre, ubicacion =:ubicacion, estrellas =:estrellas WHERE id=id", nativeQuery = true)
+    void actualizarHotel(@Param("id") Integer id, @Param ("nombre") String nombre, @Param ("ubicacion") String ubicacion, @Param ("estrellas") Integer estrellas);
+
+    @Modifying 
+    @Transactional
+    @Query (value = "DELETE FROM hoteles WHERE id = :id", nativeQuery = true)
+    void eliminarHotel(@Param("id") int id);
+}
